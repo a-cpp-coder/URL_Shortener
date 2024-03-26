cf2c19543844dfafee9c59b82c621c8e323bdddc
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-26 11:27:45 +0700

    Setting up the UI

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,6 +1,9 @@
 class ViewsController < ApplicationController
   before_action :set_link
 
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to root_path, alert: "Link not found"
+  end
   def show
     @link.views.create(
       ip: request.ip,
